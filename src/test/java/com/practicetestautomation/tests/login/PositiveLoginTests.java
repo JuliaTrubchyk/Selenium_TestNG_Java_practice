887ec8af9f6d1544ced8dc7b642829bcb8f506ca
Yuliia <julia.koshovets@gmail.com>
2025-12-01 13:31:16 -0800

    Added first Assertions to verify positive login scenario in testLoginFunctionality method

--- src/test/java/com/practicetestautomation/tests/login/PositiveLoginTests.java
+++ src/test/java/com/practicetestautomation/tests/login/PositiveLoginTests.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 
 public class PositiveLoginTests {
     public void testLoginFunctionality(){
@@ -11,9 +12,6 @@ public class PositiveLoginTests {
         // Type username student into Username field
         // Type password Password123 into Password field
         // Push Submit button
-        // Verify new page URL contains practicetestautomation.com/logged-in-successfully/
-        // Verify new page contains expected text ('Congratulations' or 'successfully logged in')
-        // Verify button Log out is displayed on the new page
         WebDriver driver = new ChromeDriver();
         driver.get("https://practicetestautomation.com/practice-test-login/");
         WebElement usernameInput = driver.findElement(By.id("username"));
@@ -22,6 +20,21 @@ public class PositiveLoginTests {
         usernameInput.sendKeys("student");
         passwordInput.sendKeys("Password123");
         submitBtn.click();
+
+        // Verify new page URL contains practicetestautomation.com/logged-in-successfully/
+        String expectedUrl = "https://practicetestautomation.com/logged-in-successfully/";
+        String actualUrl = driver.getCurrentUrl();
+        Assert.assertEquals(actualUrl, expectedUrl);
+
+        // Verify new page contains expected text ('Congratulations' or 'successfully logged in')
+        String expectedMessage = "Congratulations student. You successfully logged in!";
+        String pageSource = driver.getPageSource();
+        Assert.assertTrue(pageSource.contains(expectedMessage));
+
+        // Verify button Log out is displayed on the new page
+        WebElement logOutBtn = driver.findElement(By.linkText("Log out"));
+        Assert.assertTrue(logOutBtn.isDisplayed());
+
         driver.quit();
     }
 }
