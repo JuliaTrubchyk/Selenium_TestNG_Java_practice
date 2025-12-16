d70ae893d45642a44f1bf37681cd2ba77207a905
Yuliia <julia.koshovets@gmail.com>
2025-12-16 15:14:47 -0800

    Enhanced the code by establishing inheritance and moving common methods to parent BasePage class

--- src/main/java/com/practicetestautomation/pageobjects/SuccessfulLoginPage.java
+++ src/main/java/com/practicetestautomation/pageobjects/SuccessfulLoginPage.java
@@ -7,22 +7,14 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
-public class SuccessfulLoginPage {
-    private WebDriver driver;
-    private WebDriverWait wait;
+public class SuccessfulLoginPage extends BasePage{
     private By logOutBtnLocator = By.linkText("Log out");
 
     public SuccessfulLoginPage(WebDriver driver){
-        this.driver = driver;
-        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        super(driver);
     }
 
     public boolean isLogOutBtnDisplayed(){
-        try {
-            return driver.findElement(logOutBtnLocator).isDisplayed();
-        } catch (NoSuchElementException e) {
-            return false;
-        }
-
+        return isDisplayed(logOutBtnLocator);
     }
 }
